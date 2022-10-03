5536fb01249d459b6bb1ba34e24f387f7e4650a0
Sergey Gulaga <ry6ens@gmail.com>
2022-10-03 22:23:19 +0300

    update ground images

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,23 +1,41 @@
+import { useState } from "react";
+
 import "./Gallery.scss";
 
+import { items } from "./items";
+
+import Modal from "../Modal/Modal";
+
 export default function Gallery() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClickModal = (e) => {
+    document.body.classList.add("no-scroll");
+    setShowModal(e.target.dataset.image);
+  };
+
+  const elements = items.map(({ id, text, imgUrl }) => (
+    <div
+      key={id}
+      className={text}
+      data-image={imgUrl}
+      onClick={handleClickModal}
+    ></div>
+  ));
+
+  const getModalImage = (showModal) => {
+    setShowModal(showModal);
+  };
+
   return (
     <section className="galleryPhotos">
       <h2 className="galleryTitle">Галерея</h2>
-      <div className="galleryList">
-        <div className="photo photo-borshch"></div>
-        <div className="photo photo-buriak"></div>
-        <div className="photo photo-cukini"></div>
-        <div className="photo photo-galushki"></div>
-        <div className="photo photo-indichka"></div>
-        <div className="photo photo-kachana"></div>
-        <div className="photo photo-knish"></div>
-        <div className="photo photo-kulish"></div>
-        <div className="photo photo-kurcha"></div>
-        <div className="photo photo-sirnuku"></div>
-        <div className="photo photo-sudak"></div>
-        <div className="photo photo-tomatoMelon"></div>
-      </div>
+      <div className="galleryList">{elements}</div>
+      {showModal && (
+        <Modal onClose={getModalImage}>
+          <img src={showModal} alt="img" className="modalImg" />
+        </Modal>
+      )}
     </section>
   );
 }
