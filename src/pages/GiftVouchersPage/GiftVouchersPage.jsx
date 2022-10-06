a153d274b8eac2c5ea7a125f752de5c46bff2fad
Sergey Gulaga <ry6ens@gmail.com>
2022-10-06 21:50:04 +0300

    update GiftVoucherPage
    add script GoogleSheet + SendEmail

--- src/pages/GiftVouchersPage/GiftVouchersPage.jsx
+++ src/pages/GiftVouchersPage/GiftVouchersPage.jsx
@@ -3,20 +3,16 @@ import { useForm, Controller } from "react-hook-form";
 import ReactSelect from "react-select";
 import PhoneInput, { isPossiblePhoneNumber } from "react-phone-number-input";
 import { ErrorMessage } from "@hookform/error-message";
-import { useDispatch } from "react-redux";
 
 import Title from "../../components/Title/Title";
 import Navigation from "../../components/Navigation/Navigation";
-import { getGoogleSheetOperations } from "../../redux/googleAuth-operations";
 
 import { ReactComponent as Warning } from "../../images/svg/warning.svg";
 
 import s from "./GiftVouchersPage.module.scss";
 import "./styles.css";
 
-export default function GiftVouchersPage() {
-  const dispatch = useDispatch();
-
+function GiftVouchersPage() {
   const isDesktop = useMediaQuery({ minWidth: 1024 });
 
   const {
@@ -35,16 +31,24 @@ export default function GiftVouchersPage() {
 
   const onSubmit = (data, e) => {
     e.preventDefault();
-    console.log(data);
-    dispatch(getGoogleSheetOperations(data));
+    fetch(
+      `https://script.google.com/macros/s/AKfycbzA3WbJybNVVOZmab4kafXd-P0Nr74XhK70-teC5pMoVCVNtjxYZuNa7U1ASM6AHnKG/exec?name=${data.name}&tel=${data.tel}&sum=${data.sum.value}`,
+      {
+        method: "POST",
+        // body: JSON.stringify(newData),
+        // body: data,
+      }
+    )
+      .then((result) => result)
+      .catch((error) => console.log("error", error));
     reset();
   };
 
   return (
-    <>
+    <main>
       <Title
         // img={KlopotenkoIMG}
-        // imgClass="imgFounders"
+        imgClass="none"
         title="Сертифікат"
         textClass="textFounders"
         textParag1="Ми дуже хочемо, аби ви мали нагоду подарувати новий гастрономічний досвід своїм близьким людям. З цією думкою ми зробили подарункові сертифікати на суми від 1 000 грн."
@@ -52,98 +56,103 @@ export default function GiftVouchersPage() {
         textParag3="Радимо не друкувати електронний сертифікат – нам достатньо переглянути його на екрані смартфону."
         btnBook={false}
       />
-      <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-        <Controller
-          name="sum"
-          control={control}
-          rules={{ required: "Обов'язкове поле" }}
-          render={({ field }) => (
-            <ReactSelect
-              placeholder="Оберіть суму ..."
-              isClearable
-              {...field}
-              options={[
-                { value: "1000", label: "1000 грн" },
-                { value: "1500", label: "1500 грн" },
-                { value: "2000", label: "2000 грн" },
-                { value: "2500", label: "2500 грн" },
-                { value: "3000", label: "3000 грн" },
-              ]}
-            />
-          )}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="sum"
-          render={({ message }) => (
-            <div className={s.errorOverlay}>
-              <Warning />
-              <p className={s.errorMessage}>{message}</p>
-            </div>
-          )}
-        />
-
-        <label htmlFor="">
-          <input
-            className={s.input}
-            {...register("name", {
-              required: "Обов'язкове поле",
-              pattern: {
-                value: /[a-z]/gi,
-                message: "Обов'язкове поле",
-              },
-            })}
-            type="text"
-            placeholder="Ім‘я та прізвище того, хто дарує"
-            // pattern="[бвгґджзклмнпрстфхцчшщйаеєиіїоуюяь]"
-            // title="4 to 8 lowercase letters"
+      <section className={s.section}>
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+          <Controller
+            name="sum"
+            control={control}
+            rules={{ required: "Обов'язкове поле" }}
+            render={({ field: { onChange, value } }) => (
+              <ReactSelect
+                placeholder="Оберіть суму ..."
+                isClearable
+                onChange={onChange}
+                value={value}
+                options={[
+                  { value: "1000", label: "1000 грн" },
+                  { value: "1500", label: "1500 грн" },
+                  { value: "2000", label: "2000 грн" },
+                  { value: "2500", label: "2500 грн" },
+                  { value: "3000", label: "3000 грн" },
+                ]}
+              />
+            )}
           />
-        </label>
-        <ErrorMessage
-          errors={errors}
-          name="name"
-          render={({ message }) => (
-            <div className={s.errorOverlay}>
-              <Warning />
-              <p className={s.errorMessage}>{message}</p>
-            </div>
-          )}
-        />
+          <ErrorMessage
+            errors={errors}
+            name="sum"
+            render={({ message }) => (
+              <div className={s.errorOverlay}>
+                <Warning />
+                <p className={s.errorMessage}>{message}</p>
+              </div>
+            )}
+          />
 
-        <Controller
-          control={control}
-          name="tel"
-          rules={{ required: "Обов'язкове поле" }}
-          render={({ field: { onChange, value } }) => (
-            <PhoneInput
-              // className={s.input}
-              value={value}
-              placeholder="(99) 999-99-99"
-              name={"query"}
-              control={control}
-              onChange={onChange}
-              defaultCountry="UA"
-              international
-              error={value && isPossiblePhoneNumber(value) ? "true" : "false"}
+          <label htmlFor="">
+            <input
+              className={s.input}
+              {...register("name", {
+                required: "Обов'язкове поле",
+                pattern: {
+                  value: /[A-Za-z]|[бвгґджзклмнпрстфхцчшщйаеєиіїоуюяь]/,
+                  message: "Обов'язкове поле",
+                },
+              })}
+              type="text"
+              placeholder="Ім‘я та прізвище того, хто дарує"
             />
-          )}
-        />
-        <ErrorMessage
-          errors={errors}
-          name="tel"
-          render={({ message }) => (
-            <div className={s.errorOverlay}>
-              <Warning />
-              <p className={s.errorMessage}>{message}</p>
-            </div>
-          )}
-        />
+          </label>
+          <ErrorMessage
+            errors={errors}
+            name="name"
+            render={({ message }) => (
+              <div className={s.errorOverlay}>
+                <Warning />
+                <p className={s.errorMessage}>{message}</p>
+              </div>
+            )}
+          />
 
-        <button className={s.btn} type="submit">
-          Подарувати
-        </button>
-      </form>
+          <Controller
+            control={control}
+            name="tel"
+            rules={{ required: "Обов'язкове поле" }}
+            render={({ field: { onChange, value } }) => (
+              <PhoneInput
+                // className={s.input}
+                maxLength="16"
+                value={value}
+                placeholder="(99) 999-99-99"
+                name={"query"}
+                control={control}
+                onChange={onChange}
+                defaultCountry="UA"
+                international
+                error={value && isPossiblePhoneNumber(value) ? "true" : "false"}
+              />
+            )}
+          />
+          <ErrorMessage
+            errors={errors}
+            name="tel"
+            render={({ message }) => (
+              <div className={s.errorOverlay}>
+                <Warning />
+                <p className={s.errorMessage}>{message}</p>
+              </div>
+            )}
+          />
+
+          <button className={s.btn} type="submit">
+            Подарувати
+          </button>
+        </form>
+      </section>
+
       {isDesktop && <Navigation navClass="footer-navigation_desktop" />}
-    </>
+    </main>
   );
 }
+
+export default GiftVouchersPage;
