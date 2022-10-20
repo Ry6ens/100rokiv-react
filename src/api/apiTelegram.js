1a725811618194482a8598496f6182783898a720
Sergey Gulaga <ry6ens@gmail.com>
2022-10-20 14:53:33 +0300

    app

--- src/api/apiTelegram.js
+++ src/api/apiTelegram.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const axiosTelegram = async (formData) => {
+  console.log(formData);
   const { data } = axios.post(`/api/telegram`, formData);
   return data;
 };
